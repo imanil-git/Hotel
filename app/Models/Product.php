097bac589a6bb597bfefd26ecbaf9b64038afbad
Anil Rumdali Rai <anil.rai704757@gmail.com>
2024-02-04 18:19:05 +0545

    model and resource

---
+++ app/Models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'price',
+        'description',
+        'image',
+        'category_id'
+    ];
+
+    /**
+     * Get the category that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class,);
+    }
+}
